f13efc1f3a4af9c45e7b520fdb9d1bc139875448
Adam Hirny <karol.nowak0490@protonmail.com>
2024-12-10 20:49:09 +0100

    feat: durther work on app

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -3,18 +3,24 @@ import { HeaderComponents } from './header/header.component';
 import { UserComponent } from './user/user.component';
 
 import { DUMMY_USERS } from './dumy_user';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponents, UserComponent, NgFor],
+  imports: [HeaderComponents, UserComponent, CommonModule, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   protected users = DUMMY_USERS;
+  protected selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onUserReceived(id: string) {
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
