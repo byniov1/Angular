1dfa39641e1116e72b5b72387206aaeb9bc5f691
Adam Hirny <karol.nowak0490@protonmail.com>
2024-12-10 18:46:02 +0100

    feat: inputs on signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, Input, signal, input } from '@angular/core';
 
 import { DUMMY_USERS } from '../dumy_user';
 
@@ -14,24 +14,37 @@ let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
   // selectedUser = DUMMY_USERS[randomIndex];
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
+
   /* get imagePath() {
     // return `assets/users/` + this.selectedUser.avatar;
     // return `assets/users/` + this.selectedUser().avatar;
   } */
+
   // protected imagePath = computed(
   //   () => `assets/users/` + this.selectedUser().avatar
   // );
+
   // onSelectUser() {
   //   randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   //   // this.selectedUser = DUMMY_USERS[randomIndex];
   //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
   // }
 
+  /* - - - - - - - - - - - - - - - - - -  
+  // Classic input
   @Input({ required: true }) avatar: string = '';
 
   @Input({ required: true }) name: string = '';
 
   get imagePath() {
     return `assets/users/` + this.avatar;
-  }
+  } 
+  - - - - - - - - - - - - - - - - - - */
+
+  // signal as Inputs()
+  public avatar = input.required<string>();
+
+  public name = input<string>('');
+
+  protected imagePath = computed(() => `assets/users/` + this.avatar());
 }
