f5cfe5ceddf7df955b357535b26dc5a9f64a2311
Adam Hirny <adam.hirny@comarch.com>
2024-12-10 18:12:00 +0100

    init commit

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component, computed, signal } from '@angular/core';
+
+import { DUMMY_USERS } from '../dumy_user';
+
+let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  // selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  /* get imagePath() {
+    // return `assets/users/` + this.selectedUser.avatar;
+    // return `assets/users/` + this.selectedUser().avatar;
+  } */
+
+  protected imagePath = computed(
+    () => `assets/users/` + this.selectedUser().avatar
+  );
+
+  onSelectUser() {
+    randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
