a06e9ccce0ed1ad55990a8079dbea3e78cf8e5f0
Adam Hirny <karol.nowak0490@protonmail.com>
2024-12-10 18:24:32 +0100

    feat: implementation of Inputs in children component

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponents } from './header/header.component';
 import { UserComponent } from './user/user.component';
 
+import { DUMMY_USERS } from './dumy_user';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponents, UserComponent],
+  imports: [HeaderComponents, UserComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected users = DUMMY_USERS;
+}
