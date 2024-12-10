314160f6de78e7553a85bb80ab04bf228dad8101
Adam Hirny <adam.hirny@comarch.com>
2024-12-10 18:29:21 +0100

    chore: code refactor

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -27,9 +27,9 @@ export class UserComponent {
   //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
   // }
 
-  @Input() avatar: string = '';
+  @Input({ required: true }) avatar: string = '';
 
-  @Input() name: string = '';
+  @Input({ required: true }) name: string = '';
 
   get imagePath() {
     return `assets/users/` + this.avatar;
