59358804475cc7d98ced46d5fc0fad64b2bbc765
Bynio <karol.nowak0490@protonmail.com>
2024-12-15 00:54:18 +0100

    feat: create add new task component

--- 01-starting-project/src/app/tasks/new-tasks/new-tasks.component.ts
+++ 01-starting-project/src/app/tasks/new-tasks/new-tasks.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../tasks.model';
 
 @Component({
   selector: 'app-new-tasks',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-tasks.component.html',
   styleUrl: './new-tasks.component.css'
 })
 export class NewTasksComponent {
+  @Output() cancelClicked = new EventEmitter<void>();
+  
+  @Output() addNewTask = new EventEmitter<NewTaskData>();
 
+  protected newTask = signal<NewTaskData>({
+    title: '',
+    summary: '',
+    date: '',
+  })
+
+  // protected newTask = {
+  //   title: '',
+  //   summary: '',
+  //   date: '',
+  // }
+  
+  onCancelClicked(){
+    this.cancelClicked.emit()
+  }
+
+  onSubmit() {
+    this.addNewTask.emit(this.newTask())
+  }
 }
