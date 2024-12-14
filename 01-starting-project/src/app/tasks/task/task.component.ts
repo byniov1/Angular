107fdde9ecd14174ade98f4e97d3cf0699fb37de
Bynio <bynio788@gmail.com>
2024-12-14 10:29:19 +0100

    feat: further work on application

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from '../tasks.model';
+
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css' 
 })
+
 export class TaskComponent {
+  @Input() task?: Task;
 
 }
--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { dummyTasks } from './tasks.model';
 
 @Component({
   selector: 'app-tasks',
@@ -9,7 +10,15 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  @Input({required: true}) userId!: string
+
   @Input() name?: string | undefined;
 
+  protected tasks = dummyTasks;
+
+  get selectedUserTasks(){
+    return this.tasks.filter((task) => task.userId == this.userId)
+  }
+
   onStartAddTask() {}
-}
+} 
