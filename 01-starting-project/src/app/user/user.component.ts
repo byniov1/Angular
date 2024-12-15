9d028d3ccb40f7de32e455ae88cc0662924aaf46
Bynio <karol.nowak0490@protonmail.com>
2024-12-15 01:02:13 +0100

    feat: add card component

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,66 +1,26 @@
 import {
   Component,
-  computed,
   Input,
-  signal,
-  input,
   Output,
   EventEmitter,
-  output,
 } from '@angular/core';
 
-import { DUMMY_USERS } from '../dumy_user';
 import { User } from './user.model';
-
-// let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { CardComponent } from "../common/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // selectedUser = DUMMY_USERS[randomIndex];
-  // selectedUser = signal(DUMMY_USERS[randomIndex]);
-  /* get imagePath() {
-    // return `assets/users/` + this.selectedUser.avatar;
-    // return `assets/users/` + this.selectedUser().avatar;
-  } */
-  // protected imagePath = computed(
-  //   () => `assets/users/` + this.selectedUser().avatar
-  // );
-  // onSelectUser() {
-  //   randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  //   // this.selectedUser = DUMMY_USERS[randomIndex];
-  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  // }
-
-  /* - - - - - - - - - - - - - - - - - -
-  // signal as Inputs()
-  public avatar = input.required<string>();
-
-  public name = input<string>('');
-
-  protected imagePath = computed(() => `assets/users/` + this.avatar());
- - - - - - - - - - - - - - - - - - - */
-
-  /* - - - - - - - - - - - - - - - - - -  
- // Classic input
- - - - - - - - - - - - - - - - - - - */
-  // @Input({ required: true }) avatar: string | undefined = '';
-
-  // @Input({ required: true }) name: string | undefined = '';
-
-  // @Input({ required: true }) id: string | undefined = '';
-
   @Input({ required: true }) user!: User;
 
   @Input({required: true}) selected!: boolean;
 
   @Output() select = new EventEmitter<string>();
-  // select = output<string>();
 
   get imagePath() {
     return `assets/users/` + this.user.avatar;
