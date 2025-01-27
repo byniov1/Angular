2ae0e7a56dc89cba0c9d4281f868a032f65fb37e
Bynio <karol.nowak0490@protonmail.com>
2025-01-27 18:48:05 +0100

    feat: init work on ticket feature

--- 06-components-and-templates-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-components-and-templates-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -2,12 +2,16 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  EventEmitter,
+  output,
+  Output,
   viewChild,
   ViewChild,
 } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
+import { Ticket } from '../ticket/ticket.model';
 
 @Component({
   selector: 'app-new-ticket',
@@ -20,10 +24,14 @@ export class NewTicketComponent implements AfterViewInit {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+  // @Output() add = new EventEmitter<{ title: string; text: string }>();
+  add = output<{ title: string; text: string }>();
+
   // onSubmit(titleElement: string, textElement: string, form: HTMLFormElement){
   onSubmit(titleElement: string, textElement: string) {
     console.dir(titleElement, textElement);
 
+    this.add.emit({ title: titleElement, text: textElement });
     this.form()?.nativeElement.reset();
   }
 
