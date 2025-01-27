695b7ced387963b0bac9e1c8248a6a56941c8f42
Bynio <karol.nowak0490@protonmail.com>
2025-01-27 16:07:12 +0100

    add some dynamics to server component

--- 06-components-and-templates-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-components-and-templates-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+type Status = 'online' | 'offline' | 'unknown'
+
+enum StatusEnum {
+  Online = 'online',
+  Offline = 'offline',
+  Unknow = 'unknown'
+}
+
 @Component({
   selector: 'app-server-status',
   standalone: true,
@@ -7,6 +15,35 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
+
 export class ServerStatusComponent {
-  protected currentStatus = 'online';
+  protected currentStatus: Status = 'online';
+
+  constructor(){
+    this.changeTime()
+  }
+
+  changeTime(){
+
+    setInterval(() => {
+      // if(this.currentStatus === 'online'){
+      //   this.currentStatus = 'offline'
+      // } else if(this.currentStatus === 'offline') {
+      //   this.currentStatus = 'unknown'
+      // } else {
+      //   this.currentStatus = 'online'
+      // }
+
+      const rnd = Math.random();
+
+      if(rnd < 0.5){
+        this.currentStatus = StatusEnum.Online
+      } else if (rnd < 0.9) {
+        this.currentStatus = StatusEnum.Offline
+      } else {
+        this.currentStatus = StatusEnum.Unknow
+      }
+
+    }, 5000)
+  }
 }
