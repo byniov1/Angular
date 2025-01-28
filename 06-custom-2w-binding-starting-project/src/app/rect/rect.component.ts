7d237e1e6552fdb5aaafa688382caf0bb7bdff9d
Bynio <karol.nowak0490@protonmail.com>
2025-01-28 12:11:45 +0100

    feat: learn about custom 2 way data bindings in old and new way

--- 06-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 06-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
+import { signalUpdateFn } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'app-rect',
@@ -10,7 +11,24 @@ import { Component } from '@angular/core';
 export class RectComponent {
   // Todo: Implement custom two-way binding
 
+  // Old way
+  // size = input.required<{ width: string; height: string }>();
+  // sizeChange = output<{ width: string; height: string }>();
+
+  // onReset() {
+  //   this.sizeChange.emit({
+  //     width: '200',
+  //     height: '100',
+  //   });
+  // }
+
+  //  New Way
+  size = model.required<{ width: string; height: string }>();
+
   onReset() {
-    // ...
+    this.size.set({
+      width: '200',
+      height: '100',
+    });
   }
 }
