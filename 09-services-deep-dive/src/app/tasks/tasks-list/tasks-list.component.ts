fd9eb31c5c2cbb4b68e08b169c8045993ee9ea38
Bynio <karol.nowak0490@protonmail.com>
2025-01-28 18:56:15 +0100

    feat: create and use tasksService

--- 09-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
+++ 09-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,8 +11,11 @@ import { TaskItemComponent } from './task-item/task-item.component';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
+  private tasksService = inject(TasksService);
+
+  public selectedFilter = signal<string>('all');
+
+  public tasks = this.tasksService.allTasks;
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
