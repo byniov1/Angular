48deb977eecfd87a9433064770b9cd6b1cbbb657
Bynio <karol.nowak0490@protonmail.com>
2025-01-30 18:29:30 +0100

    feat: add select status functionality to tasks-list component, write update status method in status service

--- 09-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
+++ 09-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -13,9 +13,27 @@ import { TasksService } from '../tasks.service';
 export class TasksListComponent {
   private tasksService = inject(TasksService);
 
-  public selectedFilter = signal<string>('all');
+  private selectedFilter = signal<string>('all');
 
-  public tasks = this.tasksService.allTasks;
+  // public tasks = this.tasksService.allTasks;
+  public tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'OPEN');
+      case 'in-progress':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'DONE');
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
