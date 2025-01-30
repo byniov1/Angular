515e453b7779674de9702dd715e68123709da79a
Bynio <karol.nowak0490@protonmail.com>
2025-01-30 19:36:47 +0100

    feat: inject serwices using injection token

--- 09-services-deep-dive/src/app/tasks/new-task/new-task.component.ts
+++ 09-services-deep-dive/src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TasksService } from '../tasks.service';
 import { TasksServiceToken } from '../../../main';
@@ -14,7 +20,12 @@ export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
   // private tasksService = inject(TasksService);
-  private tasksService = inject<TasksService>(TasksServiceToken);
+  // private tasksService = inject<TasksService>(TasksServiceToken);
+
+  constructor(
+    @Inject(TasksServiceToken)
+    private tasksService: TasksService
+  ) {}
 
   onAddTask(title: string, description: string) {
     this.tasksService.addTask({ title, description });
